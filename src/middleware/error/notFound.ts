bff9582c0c8656eabdd1cd8c47079fad25155c2c
abdisetiakawan <bedikadiryt@gmail.com>
2024-11-09 12:57:35 +0700

    feat: add error handling include error types

---
+++ src/middleware/error/notFound.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from 'express'
+import { AppError } from '@/utils/AppError'
+
+export const notFound = (req: Request, res: Response, next: NextFunction): void => {
+  next(AppError(`BOOM! 💥💥 Can't find ${req.originalUrl} on this server!`, 404))
+}
