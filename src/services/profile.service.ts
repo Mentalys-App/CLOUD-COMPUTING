5960a8d46c94a7f900ab39d95d4df8b3c9204e14
abdisetiakawan <bedikadiryt@gmail.com>
2024-11-13 14:14:17 +0700

    feat: Added update profile controller and route, updated profile service and types

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -1,5 +1,6 @@
 import { IProfile } from '@/types/profile.type'
 import prisma from '@/utils/client'
+import { Profile } from '@prisma/client'
 
 export const ProfileService = {
   async createProfile(profileData: IProfile): Promise<IProfile> {
@@ -9,6 +10,16 @@ export const ProfileService = {
     return {
       ...profile,
       profile_pic: profile.profile_pic ?? undefined
+    }
+  },
+  async updateProfile(userId: string, updateData: Profile): Promise<IProfile> {
+    const updatedProfile = await prisma.profile.update({
+      where: { userId },
+      data: updateData
+    })
+    return {
+      ...updatedProfile,
+      profile_pic: updatedProfile.profile_pic ?? undefined
     }
   }
 }
