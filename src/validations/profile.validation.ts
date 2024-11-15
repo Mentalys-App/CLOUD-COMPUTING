1e9014d448469183d07f48a768cd6f502a9958aa
abdisetiakawan <bedikadiryt@gmail.com>
2024-11-15 15:04:19 +0700

    refactor: Update profile validation schemas
    - Refined profileValidationSchema for consistency and added ISO date validation for birth_date.
    - Improved error messages for clarity.
    - Introduced updateProfileValidationSchema for optional profile updates.
    - Removed duplicate definitions to streamline validation logic.
    
    These changes enhance the validation process and user experience.

--- src/validations/profile.validation.ts
+++ src/validations/profile.validation.ts
@@ -16,9 +16,10 @@ export const profileValidationSchema = Joi.object({
     'string.base': 'Full name must be a string',
     'string.empty': 'Full name cannot be empty'
   }),
-  birth_date: Joi.date().required().messages({
+  birth_date: Joi.date().iso().required().messages({
     'any.required': 'Birth date is required',
-    'date.base': 'Birth date must be a valid date'
+    'date.base': 'Birth date must be a valid date',
+    'date.format': 'Birth date must be in the format YYYY-MM-DD'
   }),
   location: Joi.string().required().messages({
     'any.required': 'Location is required',
@@ -27,6 +28,28 @@ export const profileValidationSchema = Joi.object({
   }),
   gender: Joi.string().valid('MALE', 'FEMALE').required().messages({
     'any.required': 'Gender is required',
+    'string.base': 'Gender must be a string',
+    'any.only': 'Gender must be either MALE or FEMALE'
+  })
+})
+
+export const updateProfileValidationSchema = Joi.object({
+  username: Joi.string().pattern(/^\S+$/).optional().messages({
+    'string.base': 'Username must be a string',
+    'string.pattern.base': 'Username must be a single word without spaces'
+  }),
+  profile_pic: Joi.string().optional().allow(null), // Memungkinkan null
+  full_name: Joi.string().optional().messages({
+    'string.base': 'Full name must be a string'
+  }),
+  birth_date: Joi.date().iso().optional().messages({
+    'date.base': 'Birth date must be a valid date',
+    'date.format': 'Birth date must be in the format YYYY-MM-DD'
+  }),
+  location: Joi.string().optional().messages({
+    'string.base': 'Location must be a string'
+  }),
+  gender: Joi.string().valid('MALE', 'FEMALE').optional().messages({
     'string.base': 'Gender must be a string',
     'any.only': 'Gender must be either MALE or FEMALE'
   })
