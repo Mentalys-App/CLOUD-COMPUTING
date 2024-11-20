be69dc7c45932d5e8bbd10d1934c0cdeb3d58d29
abdisetiakawan <bedikadiryt@gmail.com>
2024-11-20 15:38:42 +0700

    fix(errorhandling): Moved username uniqueness check from profile service to profile controller

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -1,19 +1,9 @@
 import { db } from '@/config/firebaseConfig'
-import { doc, setDoc, collection, query, where, getDocs } from 'firebase/firestore'
+import { doc, setDoc } from 'firebase/firestore'
 import { ProfileRequestBody } from '@/types/profile.type'
-import { AppError } from '@/utils/AppError'
 
 export const profileService = {
   async createProfile(uid: string, profileData: ProfileRequestBody) {
-    const usernameQuery = query(
-      collection(db, 'profiles'),
-      where('username', '==', profileData.username)
-    )
-    const usernameSnapshot = await getDocs(usernameQuery)
-
-    if (!usernameSnapshot.empty) {
-      return AppError('Username already taken', 400)
-    }
     const profileRef = doc(db, 'profiles', uid)
     const timestamp = new Date().toISOString()
     const data = {
