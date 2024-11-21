723fbb7be92971df4f8e4f16dca13928043b5d71
abdisetiakawan <bedikadiryt@gmail.com>
2024-11-21 09:56:11 +0700

    Feat(firestore): Add user-specific prediction logging to Firestore
    - Integrated Firestore logging for user predictions:
      - Quiz predictions are now saved under `user_predictions/{uid}/quiz_requests`.
      - Audio predictions are stored under `user_predictions/{uid}/audio_requests`.
    - Updated `handleQuizPrediction` and `handleAudioPrediction` to use the authenticated user UID.
    - Enhanced `mlService` to include Firestore data saving functionality for both quiz and audio predictions.

--- src/services/ml.service.ts
+++ src/services/ml.service.ts
@@ -1,16 +1,24 @@
 import axios, { AxiosResponse } from 'axios'
 import { QuizInputData, AudioInputData, PredictionResponse } from '@/types/ml.type'
 import { MLConfig } from '@/config/ml.config'
+import { collection, doc, setDoc } from 'firebase/firestore'
+import { db } from '@/config/firebase.config'
 
 export const mlService = {
-  async sendQuizPrediction(inputData: QuizInputData): Promise<PredictionResponse> {
+  async sendQuizPrediction(uid: string, inputData: QuizInputData): Promise<PredictionResponse> {
     const response: AxiosResponse<PredictionResponse> = await axios.post(
       MLConfig.QUIZ_MODEL_API_URL,
       inputData
     )
+    const userPredictionsRef = collection(db, `user_predictions/${uid}/quiz_requests`)
+    await setDoc(doc(userPredictionsRef), {
+      inputData,
+      prediction: response.data,
+      timestamp: new Date().toISOString()
+    })
     return response.data
   },
-  async sendAudioPrediction(audioData: AudioInputData): Promise<PredictionResponse> {
+  async sendAudioPrediction(uid: string, audioData: AudioInputData): Promise<PredictionResponse> {
     const formData = new FormData()
     formData.append(
       'audio',
@@ -25,6 +33,12 @@ export const mlService = {
         headers: { 'Content-Type': 'multipart/form-data' }
       }
     )
+    const userPredictionsRef = collection(db, `user_predictions/${uid}/audio_requests`)
+    await setDoc(doc(userPredictionsRef), {
+      prediction: response.data,
+      timestamp: new Date().toISOString()
+    })
+
     return response.data
   }
 }
