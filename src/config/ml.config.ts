1c524965af4353f0cdd147ddea2f627a7c349661
abdisetiakawan <bedikadiryt@gmail.com>
2024-11-21 07:36:56 +0700

    feat: add ml config for api url

---
+++ src/config/ml.config.ts
@@ -0,0 +1,7 @@
+if (!process.env.QUIZ_MODEL_API_URL || !process.env.AUDIO_MODEL_API_URL) {
+  throw new Error('QUIZ_MODEL_API_URL and AUDIO_MODEL_API_URL must be set')
+}
+export const MLConfig = {
+  QUIZ_MODEL_API_URL: process.env.QUIZ_MODEL_API_URL,
+  AUDIO_MODEL_API_URL: process.env.AUDIO_MODEL_API_URL
+}
