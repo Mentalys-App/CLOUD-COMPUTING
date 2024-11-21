c16577942898fc089d21b9794321d3f61d61e75f
abdisetiakawan <bedikadiryt@gmail.com>
2024-11-21 09:14:37 +0700

    refactor: remove `AuthenticatedRequest` interface and simplify request handling
    - Removed the `AuthenticatedRequest` interface from `profile.route.ts` to reduce redundancy.
    - Streamlined route definition for better maintainability and clarity.

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -1,13 +1,8 @@
 import { Router, Request, Response, NextFunction } from 'express'
-import { createProfile } from '../controllers/profile.controller'
+import { AuthenticatedRequest, createProfile } from '../controllers/profile.controller'
 import { authenticateUser } from '../middleware/auth.middleware'
 
 const profileRouter: Router = Router()
-export interface AuthenticatedRequest extends Request {
-  user: {
-    uid: string
-  }
-}
 
 profileRouter.post(
   '/create',
