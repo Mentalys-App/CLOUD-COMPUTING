11752c85ac9124d332ad4875e29650b178fc5511
abdisetiakawan <bedikadiryt@gmail.com>
2024-11-22 07:45:25 +0700

    feat(api ml): Added check for HANDWRITING_MODEL_API_URL environment variable in MLConfig.

--- src/config/ml.config.ts
+++ src/config/ml.config.ts
@@ -1,4 +1,8 @@
-if (!process.env.QUIZ_MODEL_API_URL || !process.env.AUDIO_MODEL_API_URL) {
+if (
+  !process.env.QUIZ_MODEL_API_URL ||
+  !process.env.AUDIO_MODEL_API_URL ||
+  !process.env.HANDWRITING_MODEL_API_URL
+) {
   throw new Error('QUIZ_MODEL_API_URL and AUDIO_MODEL_API_URL must be set')
 }
 export const MLConfig = {
