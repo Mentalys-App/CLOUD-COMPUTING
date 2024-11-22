c2883cf02d44b57aea19036cbd7502d230624174
abdisetiakawan <bedikadiryt@gmail.com>
2024-11-22 13:50:20 +0700

    feat(docs): add REST API documentation for auth profile and 3 machine learning models

--- src/middleware/web.ts
+++ src/middleware/web.ts
@@ -1,5 +1,6 @@
 import express, { type Application, type Request, type Response } from 'express'
 import appMiddleware from '../middleware'
+import { swaggerMiddleware } from './swagger'
 
 const web: Application = express()
 web.get('/', (req: Request, res: Response) => {
@@ -8,6 +9,7 @@ web.get('/', (req: Request, res: Response) => {
     message: 'Mentalys REST API running!'
   })
 })
+swaggerMiddleware(web)
 web.use(appMiddleware)
 
 export default web
