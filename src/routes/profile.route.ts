4493e4795b1c54170f52b1b732eef33bdd4addea
abdisetiakawan <bedikadiryt@gmail.com>
2024-12-04 17:39:36 +0700

    refactor(profile.route): streamline profile routes for clarity
    - Combined profile update and get routes into a single endpoint ('/').
    - Removed the '/create' route as profile creation is managed within the controller.
    - Kept authentication and file upload middleware for both routes.

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -1,24 +1,13 @@
 import { Router, Request, Response, NextFunction } from 'express'
-import { createProfile, getProfile, updateProfile } from '../controllers/profile.controller'
+import { getProfile, updateProfile } from '../controllers/profile.controller'
 import { authenticateUser } from '../middleware/auth.middleware'
 import { AuthenticatedRequest } from '../types/AuthenticatedRequest.type'
 import { uploadProfileImage } from '../utils/uploadProfile'
 
 const profileRouter: Router = Router()
 
-profileRouter.post(
-  '/create',
-  (req: Request, res: Response, next: NextFunction) => {
-    authenticateUser(req, res, next)
-  },
-  uploadProfileImage,
-  (req: Request, res: Response, next: NextFunction) => {
-    createProfile(req as AuthenticatedRequest, res, next)
-  }
-)
-
 profileRouter.put(
-  '/update',
+  '/',
   (req: Request, res: Response, next: NextFunction) => {
     authenticateUser(req, res, next)
   },
@@ -29,7 +18,7 @@ profileRouter.put(
 )
 
 profileRouter.get(
-  '/profile',
+  '/',
   (req: Request, res: Response, next: NextFunction) => {
     authenticateUser(req, res, next)
   },
