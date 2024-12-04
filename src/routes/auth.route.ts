ad8162d6f9f3ea53b21685779363cf8a8d85b33d
abdisetiakawan <bedikadiryt@gmail.com>
2024-12-04 17:44:50 +0700

    refactor(auth.route): enhance authentication routes and add sign-out functionality
    - Updated `registerUser` and `loginUser` routes to use `AuthRequestBody` for request validation.
    - Added a new `/signout` route to allow users to sign out.
    - Maintained `reset-password` route and firebase error handling middleware.

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,13 +1,14 @@
 import { NextFunction, Request, Response, Router } from 'express'
-import { loginUser, registerUser, resetPassword } from '../controllers/auth.controller'
+import { loginUser, registerUser, resetPassword, signOutUser } from '../controllers/auth.controller'
 import firebaseErrorHandler from '../middleware/firebaseErrorHandler'
+import { AuthRequestBody } from '../types/authRequest.type'
 
 const authRouter: Router = Router()
 
-authRouter.post('/register', (req: Request, res: Response, next: NextFunction) => {
+authRouter.post('/register', (req: Request<AuthRequestBody>, res: Response, next: NextFunction) => {
   registerUser(req, res, next)
 })
-authRouter.post('/login', (req: Request, res: Response, next: NextFunction) => {
+authRouter.post('/login', (req: Request<AuthRequestBody>, res: Response, next: NextFunction) => {
   loginUser(req, res, next)
 })
 authRouter.post(
@@ -16,6 +17,14 @@ authRouter.post(
     resetPassword(req, res, next)
   }
 )
+authRouter.post('/signout', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    signOutUser(req, res, next)
+  } catch (error) {
+    next(error)
+  }
+})
+
 authRouter.use(firebaseErrorHandler)
 
 export default authRouter
