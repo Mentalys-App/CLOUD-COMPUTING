a810289f9c3b59c0ef7c6984bc4b0e69c61c1b5c
abdisetiakawan <bedikadiryt@gmail.com>
2024-12-04 17:34:45 +0700

    refactor(validation): enhance profile schemas with additional fields and rules
    - Added 'firstName', 'lastName', and 'phoneNumber' fields to the 'profileValidationSchema'.
    - Updated 'profileUpdateValidationSchema' to include new optional fields.
    - Adjusted validation rules for improved flexibility and clarity.

--- src/validations/profile.validation.ts
+++ src/validations/profile.validation.ts
@@ -10,30 +10,57 @@ export const profileValidationSchema = Joi.object({
     'string.pattern.base': 'Username must be a single word without spaces'
   }),
   profile_pic: Joi.string().optional().allow(null),
-  full_name: Joi.string().required().min(3).max(50).trim().messages({
+  firstName: Joi.string().required().min(3).max(25).trim().messages({
+    'any.required': 'First name is required',
+    'string.min': 'First name must be at least 3 characters long',
+    'string.max': 'First name must be at most 50 characters long',
+    'string.base': 'First name must be a string',
+    'string.empty': 'First name cannot be empty'
+  }),
+  lastName: Joi.string().required().min(3).max(25).trim().messages({
+    'any.required': 'Last name is required',
+    'string.min': 'Last name must be at least 3 characters long',
+    'string.max': 'Last name must be at most 50 characters long',
+    'string.base': 'Last name must be a string',
+    'string.empty': 'Last name cannot be empty'
+  }),
+  full_name: Joi.string().required().min(3).max(60).trim().messages({
     'string.min': 'Full name must be at least 3 characters long',
     'string.max': 'Full name must be at most 50 characters long',
     'any.required': 'Full name is required',
     'string.base': 'Full name must be a string',
     'string.empty': 'Full name cannot be empty'
   }),
-  birth_date: Joi.date().max('now').required().messages({
+  birth_date: Joi.date().max('now').optional().messages({
     'date.max': 'Birth date is not valid',
     'any.required': 'Birth date is required'
   }),
-  location: Joi.string().required().trim().messages({
-    'any.required': 'Location is required',
-    'string.base': 'Location must be a string',
-    'string.empty': 'Location cannot be empty'
-  }),
-  gender: Joi.string().valid('MALE', 'FEMALE').required().messages({
+  location: Joi.string().optional().trim(),
+  gender: Joi.string().valid('MALE', 'FEMALE').optional().messages({
     'any.required': 'Gender is required',
     'string.base': 'Gender must be a string',
     'any.only': 'Gender must be either MALE or FEMALE'
-  })
+  }),
+  phoneNumber: Joi.string()
+    .pattern(/^\+[1-9]\d{1,14}$/)
+    .required()
+    .messages({
+      'string.pattern.base': 'Format nomor telepon harus dimulai dengan + dan diikuti kode negara',
+      'any.required': 'Nomor telepon harus diisi'
+    })
 })
 
 export const profileUpdateValidationSchema = profileValidationSchema.fork(
-  ['username', 'full_name', 'birth_date', 'location', 'gender'],
+  [
+    'username',
+    'firstName',
+    'lastName',
+    'birth_date',
+    'location',
+    'gender',
+    'full_name',
+    'phoneNumber',
+    'profile_pic'
+  ],
   (schema) => schema.optional()
 )
