05761da418897fc42e34553a2403366986a1c1da
abdisetiakawan <bedikadiryt@gmail.com>
2024-12-04 17:42:42 +0700

    refactor(profile.type): update profile interface with new fields
    - Replaced `profile_pic` with required `profile_pic: string` in the `ProfileRequestBody` interface.
    - Added `firstName`, `lastName`, and `phoneNumber` to the `ProfileRequestBody` interface to better represent user details.
    - Introduced a new interface `ProfileRequestRegisterBody` for user registration with optional `birth_date`, `location`, and `gender` fields.

--- src/types/profile.type.ts
+++ src/types/profile.type.ts
@@ -1,8 +1,22 @@
 export interface ProfileRequestBody {
   username: string
-  profile_pic?: string
+  profile_pic: string
+  firstName: string
+  lastName: string
   full_name: string
+  phoneNumber: string
   birth_date: string
   location: string
   gender: 'MALE' | 'FEMALE'
+}
+
+export interface ProfileRequestRegisterBody {
+  username: string
+  firstName: string
+  lastName: string
+  full_name: string
+  phoneNumber: string
+  birth_date?: string
+  location?: string
+  gender?: 'MALE' | 'FEMALE'
 }
