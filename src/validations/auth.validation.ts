6cf191e0153e0f8768b5dd71f29bcca3abc1d1c9
abdisetiakawan <bedikadiryt@gmail.com>
2024-12-04 17:41:55 +0700

    refactor(profile.service): modify profile creation and update flow
    - Changed `createProfile` to use `ProfileRequestRegisterBody` for registration data without requiring image upload.
    - Simplified profile creation by removing image upload logic in `createProfile` method.
    - Updated `updateProfile` to set `full_name` based on first and last name, and retain existing values if not provided.
    - Adjusted the `updated_at` timestamp and profile data merging in the `updateProfile` method.

--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -3,7 +3,16 @@ import Joi from 'joi'
 export const registrationSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(8).required(),
-  confirmPassword: Joi.string().valid(Joi.ref('password')).required()
+  confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
+  username: Joi.string().pattern(/^\S+$/).required().trim().min(5).max(30),
+  firstName: Joi.string().min(3).max(50).required(),
+  lastName: Joi.string().min(3).max(50).required(),
+  phoneNumber: Joi.string()
+    .pattern(/^\+[1-9]\d{1,14}$/)
+    .required(),
+  birth_date: Joi.date().max('now').optional(),
+  location: Joi.string().optional().trim(),
+  gender: Joi.string().valid('MALE', 'FEMALE').optional()
 })
 
 export const phoneLoginSchema = Joi.object({
